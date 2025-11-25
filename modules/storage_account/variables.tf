7e2e006253ba9435a97337bee1f1f18ebab05812
Khushi-Gupta27 <khushigu@hcltech.com>
2025-11-25 13:09:14 +0530

    Update variables.tf

--- modules/storage_account/variables.tf
+++ modules/storage_account/variables.tf
@@ -2,3 +2,4 @@ variable "sa_name" { type = string }
 variable "location" { type = string }
 variable "resource_group" { type = string }
 variable "account_replication_type" {type = string }
+variable "webapp_id" {type = string }
